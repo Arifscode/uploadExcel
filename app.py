dd22c430f33a10a7594099790d2d6f5e6d07a609
unknown <Arif Hussain>
2024-05-18 17:02:58 +0530

    app file changed

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
 
-# @app.route('/data', methods=['GET', 'POST'])
-# def data():
-#     if request.method == 'POST':
-#         file = request.form['upload-file']
-#         data = pd.read_excel(file)
-#         return render_template('data.html', data=data.to_dict())
+@app.route('/')
+def hello_world():
+    return 'Hello from Koyeb'
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
